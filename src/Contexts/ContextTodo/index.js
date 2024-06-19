5b38ec7daa1d0a6586f0731a22bd6d98b03ed6cb
JaishreeNishad <Jaysh2222@gmail.com>
2024-06-19 14:20:36 +0530

    Export useTodo for access TodoContext

--- src/Contexts/ContextTodo/index.js
+++ src/Contexts/ContextTodo/index.js
@@ -1,5 +1,36 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const TodoContext = createContext();
 
-export const TodoProvider = ({ children }) => {};
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodos = (text) => {
+    const newTodo = { text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return {
+          ...todos,
+          completed: !todos.completed,
+          completedAt: !todos.completed ? new Date().toISOString() : null,
+        };
+      }
+      return todos;
+    });
+    setTodos(newTodos);
+  };
+  return (
+    <TodoContext.Provider vaue={{ todos, addTodos, toggleTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export const useTodos = () => {
+  const context = useContext(TodoContext);
+  return context;
+};
